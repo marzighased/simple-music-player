af39e0813868f3aae4b45167248d01edcc64325b
Marzi Ghasedian <marzighased@gmail.com>
2025-05-01 19:57:38 +0200

    sample Musics added

---
+++ script.js
@@ -0,0 +1,14 @@
+const tracks = [
+    {
+        name: "Relaxing Music",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+    },
+    {
+        name: "Cill Vibes",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3"
+    },
+    {
+        name: "Ambient Tune",
+        url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3"
+    }
+];
