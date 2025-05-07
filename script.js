2e456aa7a2b001bbe4dc364751971b657915b83b
Marzi Ghasedian <marzighased@gmail.com>
2025-05-07 23:24:19 +0200

    Time Display function added

--- script.js
+++ script.js
@@ -68,8 +68,14 @@ function formatTime(seconds) {
     const mins = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
     return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-    
 }
+
+audio.addEventListener("timeupdate", () => {
+    const current = formatTime(audio.currentTime);
+    const total = formatTime(audio.duration);
+    timeDisplay.textContent = `${current} / ${total}`;
+
+});
 
 
 
